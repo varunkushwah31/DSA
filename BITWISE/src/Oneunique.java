5fdc83723914bc6f560c914c7d5502f6eaf9e463
Varun Kushwah <varun.kush3@gmail.com>
2026-03-05 23:07:05 +0530

    Improve UniqueElement

--- BITWISE/src/Oneunique.java
+++ BITWISE/src/Oneunique.java
@@ -14,7 +14,7 @@ public class Oneunique {
     public static int UniqueElement(int[] arr){
         int unique = 0;
         for(int i=0;i< arr.length;i++){
-            unique ^= arr[i];
+            unique = unique ^ arr[i];
         }
         return unique;
     }
