e385a23a9b54955f9eb3b18a114b0378710f0eca
Varun Kushwah <varun.kush3@gmail.com>
2026-03-07 23:12:39 +0530

    Added ReverseInBetween

---
+++ LinkedList/src/ReverseInBetween.java
@@ -0,0 +1,76 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head == null || left == right) return head;
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+
+        // Move prev to node before left
+        for (int i = 1; i < left; i++) {
+            prev = prev.next;
+        }
+
+        ListNode curr = prev.next;
+
+        // Reverse between left and right
+        for (int i = 0; i < right - left; i++) {
+            ListNode nextNode = curr.next;
+            curr.next = nextNode.next;
+            nextNode.next = prev.next;
+            prev.next = nextNode;
+        }
+
+        return dummy.next;
+    }
+}
+
+public class ReverseInBetween {
+
+    // Function to print linked list
+    public static void printList(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        // Creating linked list: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original List:");
+        printList(head);
+
+        Solution sol = new Solution();
+        head = sol.reverseBetween(head, 2, 4);
+
+        System.out.println("After Reversing (2 to 4):");
+        printList(head);
+    }
+}
